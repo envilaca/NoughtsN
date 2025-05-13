8ebc5126ca8422d74b764237115c82d7251df6ff
eileennaganumavilaca <eileen.vilaca@ten10.com>
2025-05-13 14:55:49 +0100

    I'm unversioned? You're unversioned!

---
+++ src/main/java/noughtsn/Grid.java
@@ -0,0 +1,62 @@
+package noughtsn;
+
+public class Grid {
+
+    boolean play(int index) {
+        if (grid[index] != '\0') throw new IllegalArgumentException();
+        grid[index] = turn ? 'O' : 'X';
+        turn = !turn;
+        return index == 4 && hasThreeInADig(0)
+            || index == 4 && hasThreeInADig(2)
+            || index % 2 == 0 && hasThreeInADig(index)
+            || hasThreeInACol(index % 3)
+            || hasThreeInARow(index / 3);
+    }
+
+    public String toString() {
+        return getRow(0) +
+                "────┼────┼────" +
+                getRow(1) +
+                "────┼────┼────" +
+                getRow(2);
+    }
+
+    private String getRow(int row) {
+        return "\n " +
+                toTwo(grid[row * 3]) +
+                " │ " +
+                toTwo(grid[row * 3 + 1]) +
+                " │ " +
+                toTwo(grid[row * 3 + 2]) +
+                "\n";
+    }
+
+    private String toTwo(char symb) {
+        return switch (symb) {
+            case '\0' -> "  ";
+            case 'X'  -> "⟩⟨";
+            case 'O'  -> "()";
+            default -> throw new IllegalStateException("Unexpected value: " + symb);
+        };
+    }
+
+    private boolean hasThreeInARow(int row) {
+        return grid[row * 3] == grid[row * 3 + 1] && grid[row * 3 + 1] == grid[row * 3 + 2];
+    }
+
+    private boolean hasThreeInACol(int col) {
+        return grid[col] == grid[3 + col] && grid[3 + col] == grid[6 + col];
+    }
+
+    private boolean hasThreeInADig(int dig) {
+        return grid[dig] == grid[4] && grid[4] == grid[8 - dig];
+    }
+
+    public Grid() {
+        grid = new char[9];
+    }
+
+    char[] grid;
+    boolean turn;
+
+}
---
+++ src/test/java/noughtsn/GridTests.java
@@ -0,0 +1,99 @@
+package noughtsn;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GridTests {
+
+    @BeforeEach
+    void beforeEach() {
+        grid = new Grid();
+    }
+
+    @Test
+    void occupiedSquareUnwriteable() {
+        // Act
+        grid.play(0);
+
+        // Assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> grid.play(0));
+    }
+
+    @Test
+    void boundsApply() {
+        // Assert
+        Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, () -> grid.play(9));
+    }
+
+    @Test
+    void horizontalWinWins() {
+        // Act
+        grid.play(3);
+        grid.play(0);
+        grid.play(4);
+        grid.play(1);
+
+        // Assert
+        Assertions.assertTrue(grid.play(5));
+    }
+
+    @Test
+    void verticalWinWins() {
+        // Act
+        grid.play(1);
+        grid.play(2);
+        grid.play(4);
+        grid.play(5);
+
+        // Assert
+        Assertions.assertTrue(grid.play(7));
+    }
+
+    @Test
+    void diagonalWinWins() {
+        // Act
+        grid.play(2);
+        grid.play(0);
+        grid.play(4);
+        grid.play(8);
+
+        // Assert
+        Assertions.assertTrue(grid.play(6));
+    }
+
+    @Test
+    void gridLooksRight() {
+        // Arrange
+        grid.play(0);
+        grid.play(4);
+        grid.play(8);
+        grid.play(7);
+        grid.play(1);
+        grid.play(2);
+        grid.play(6);
+        grid.play(3);
+        grid.play(5);
+
+        // Assert
+        Assertions.assertEquals("""
+            
+             ⟩⟨ │ ⟩⟨ │ ()
+            ────┼────┼────
+             () │ () │ ⟩⟨
+            ────┼────┼────
+             ⟩⟨ │ () │ ⟩⟨
+            """,
+                grid.toString()
+        );
+    }
+
+    Grid grid;
+
+}
+
+//  ⟩⟨ │ ⟩⟨ │ ()
+// ────┼────┼────
+//  () │ () │ ⟩⟨
+// ────┼────┼────
+//  ⟩⟨ │ () │ ⟩⟨
