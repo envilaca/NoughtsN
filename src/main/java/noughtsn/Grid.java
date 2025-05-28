faf9f41daac54377ca9369d287b7777c7a70e748
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-05-28 13:27:53 +0100

    Added history and free squares to Grid. Publicised some methods.

--- src/main/java/noughtsn/Grid.java
+++ src/main/java/noughtsn/Grid.java
@@ -1,18 +1,31 @@
 package noughtsn;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static java.lang.Math.abs;
 
 public class Grid {
 
-    protected static int RWS = 3; // No. of rows.
-    protected static int CLS = 3; // No. of columns.
+    protected static final boolean DEX = true;  // Diagonal from upper left to bottom right.
+    protected static final boolean SIN = false; // Diagonal from upper right to bottom left.
+
+    protected static final int RWS = 3; // No. of rows.
+    protected static final int CLS = 3; // No. of columns.
 
     boolean play(int index) {
         if (grid[index] != 0) throw new IllegalArgumentException("Square already played.");
-        grid[index] = (byte) (turn ? +1 : -1); turn = !turn; Computer.free.remove(index);
-        return index % 2 == 0 && sameInADig() == 3
-                || sameInACol(index % RWS)    == 3
-                || sameInARow(index / CLS)    == 3;
+        grid[index] = (byte) (turn ? +1 : -1);
+        turn = !turn;
+        history.add(index);
+        free.remove(index);
+
+        return index - (CLS - 1) - (index / RWS) * (CLS - 1) == 0 && sameInADig(SIN) == 3
+                || index % (RWS * CLS / 2) == 0 && sameInADig(DEX) == 3
+                || sameInACol(index % CLS) == 3
+                || sameInARow(index / RWS) == 3;
     }
 
     public String toString() {
@@ -44,32 +57,38 @@ public class Grid {
         };
     }
 
-    private byte sameInARow(int row) {
+    public byte sameInARow(int row) {
         return (byte) abs(grid[row * RWS] + grid[row * RWS + 1] + grid[row * RWS + 2]);
     }
 
-    private byte sameInACol(int col) {
-        return (byte) abs(grid[col]       + grid[CLS + col]     + grid[CLS * 2 + col]);
+    public byte sameInACol(int col) {
+        return (byte) abs(grid[col] + grid[CLS + col] + grid[CLS * 2 + col]);
     }
 
-    // I used to check here if the square being passed in was a diagonal, using the following:
-    //                       index % (RWS * CLS / 2) == 0    for the dexter diagonal
-    // index - (CLS - 1) - (index / RWS) * (CLS - 1) == 0    for the sinister diagonal
-    // However, this is probably more intensive than just actually checking them every time, so I'm
-    // not going to do that anymore.
-    private byte sameInADig() {
-        return (byte) Math.max(abs(grid[0] + grid[4] + grid[8]), abs(grid[2] + grid[4] + grid[6]));
+    public byte sameInADig(boolean dig) {
+        return (byte) (dig ? abs(grid[0] + grid[4] + grid[8]) : abs(grid[2] + grid[4] + grid[6]));
     }
 
-    protected void clear() {
-        for (int i = 0; i < RWS * CLS; i++) grid[i] = '\0';
-    }
 
     public Grid() {
         grid = new byte[RWS * CLS];
+        history = new ArrayList<>();
+        free = new HashSet<>(Set.of(0, 1, 2, 3, 4, 5, 6, 7, 8));
     }
 
-    byte[] grid;
-    boolean turn; // X or O.
+    private final byte[] grid;  // Initialised with 0s.
+    private boolean turn; // X or O.
+
+    public List<Integer> getHistory() {
+        return history;
+    }
+
+    private final List<Integer> history;
+
+    public Set<Integer> getFree() {
+        return free;
+    }
+
+    private final Set<Integer> free;
 
 }
