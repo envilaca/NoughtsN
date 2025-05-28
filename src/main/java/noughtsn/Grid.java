bf022eda90144277022e38a8168d15812e92091e
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-05-28 09:49:36 +0100

    Grid more dynamic.

--- src/main/java/noughtsn/Grid.java
+++ src/main/java/noughtsn/Grid.java
@@ -2,14 +2,15 @@ package noughtsn;
 
 public class Grid {
 
+    protected static int RWS = 3; // No. of rows.
+    protected static int CLS = 3; // No. of columns.
+
     boolean play(int index) {
         if (grid[index] != '\0') throw new IllegalArgumentException("Square already played.");
-        grid[index] = turn ? 'O' : 'X';
-        turn = !turn;
+        grid[index] = turn ? 'O' : 'X'; turn = !turn; Computer.free.remove(index);
         return hasThreeInADig(index)
-            || hasThreeInACol(index % 3)
-            || hasThreeInARow(index / 3);
-
+            || hasThreeInACol(index % RWS)
+            || hasThreeInARow(index / CLS);
     }
 
     public String toString() {
@@ -23,11 +24,11 @@ public class Grid {
     // Makes a row string.
     private String getRow(int row) {
         return "\n " +
-                toTwo(grid[row * 3]) +
+                toTwo(grid[row * RWS]) +
                 " │ " +
-                toTwo(grid[row * 3 + 1]) +
+                toTwo(grid[row * RWS + 1]) +
                 " │ " +
-                toTwo(grid[row * 3 + 2]) +
+                toTwo(grid[row * RWS + 2]) +
                 "\n";
     }
 
@@ -42,23 +43,29 @@ public class Grid {
     }
 
     private boolean hasThreeInARow(int row) {
-        return grid[row * 3] == grid[row * 3 + 1] && grid[row * 3 + 1] == grid[row * 3 + 2];
+        return grid[row * RWS] == grid[row * RWS + 1] && grid[row * RWS + 1] == grid[row * RWS + 2];
     }
 
     private boolean hasThreeInACol(int col) {
-        return grid[col] == grid[3 + col] && grid[3 + col] == grid[6 + col];
+        return grid[col] == grid[CLS + col] && grid[CLS + col] == grid[CLS * 2 + col];
     }
 
     private boolean hasThreeInADig(int ind) {
-        return ind % 4 == 0                 && grid[0] == grid[4] && grid[4] == grid[8]
-            || ind - 2 - (ind / 3) * 2 == 0 && grid[2] == grid[4] && grid[4] == grid[6];
+        return ind % (RWS * CLS / 2) == 0                     && grid[0] == grid[4] && grid[4] == grid[8]
+            || ind - (CLS - 1) - (ind / RWS) * (CLS - 1) == 0 && grid[2] == grid[4] && grid[4] == grid[6];
+    }
+
+    protected void clear() {
+        for (int i = 0; i < RWS * CLS; i++) {
+            grid[i] = '\0';
+        }
     }
 
     public Grid() {
-        grid = new char[9];
+        grid = new char[RWS * CLS];
     }
 
     char[] grid;
-    boolean turn;
+    boolean turn; // X or O.
 
 }
