1b514716cadf270d4965120c6ac9d85aeab2b08b
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-05-28 15:02:59 +0100

    Made the computerTriesWins() test pass.

--- src/main/java/noughtsn/Computer.java
+++ src/main/java/noughtsn/Computer.java
@@ -7,24 +7,32 @@ import static noughtsn.Grid.RWS;
 
 public class Computer {
 
-    @SuppressWarnings("StatementWithEmptyBody")
     int choose() {
-        int last;
-        if (grid.getHistory().isEmpty()) last = 1;
-        else last = grid.getHistory().getLast();
-
-        int[] sq;
-        if      (grid.dexter(last) && grid.sameIn(sq = grid.dig(Grid.DEX)) == 2);
-        else if (grid.sinstr(last) && grid.sameIn(sq = grid.dig(Grid.SIN)) == 2);
-        else if (grid.sameIn(sq = grid.col(last % CLS)) == 2);
-        else if (grid.sameIn(sq = grid.row(last / RWS)) == 2);
-        else return free.toArray(new Integer[0])[new Random().nextInt(free.size())];
+        int[] sq = new int[]{free.toArray(new Integer[0])[new Random().nextInt(free.size())]};
+        for (int i : trimmedHist()) {
+            System.out.println("Doing this thing, i is " + i);
+            if      (grid.dexter(i) && grid.sameIn(sq = grid.dig(Grid.DEX)) == 2) break;
+            else if (grid.sinstr(i) && grid.sameIn(sq = grid.dig(Grid.SIN)) == 2) break;
+            else if (grid.sameIn(sq = grid.col(i % CLS)) == 2) break;
+            else if (grid.sameIn(sq = grid.row(i / RWS)) == 2) break;
+        }
         return getFree(sq);
     }
 
-    int getFree(int[] squares) {
+    private int getFree(int[] squares) {
         for (int i : squares) if (free.contains(i)) return i;
         throw new IllegalArgumentException();
+    }
+
+    // Get an array of the moves that need checking. 🦎🌳
+    private int[] trimmedHist() {
+        List<Integer> hist = grid.getHistory();
+
+        int[] moves = new int[hist.size() / 2 + 1];
+        if (hist.isEmpty()) moves = new int[0];
+        else moves[0] = hist.getLast();
+        for (int i = 1; i < moves.length; i++) moves[i] = hist.get(hist.size() - i * 2);
+        return moves;
     }
 
     public void setGrid(Grid grid) {
