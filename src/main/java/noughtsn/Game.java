37a9ba4b7d64685041a9b782ec2cf8020e73d6b2
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-06-02 15:55:47 +0100

    Made test for new input pass.

--- src/main/java/noughtsn/Game.java
+++ src/main/java/noughtsn/Game.java
@@ -11,19 +11,62 @@ public class Game implements Runnable {
     private final Grid grid;
     private final Computer computer;
 
+    private static final int INVALID_INPUT = -1;
+
     @SuppressWarnings("AssignmentUsedAsCondition")
     @Override
     public void run() {
-        String in = "";
-        boolean turn; // true = X. false = O.
+        boolean isWon = false, turn; // turn: true = X. false = O.
 
         do {
-            System.out.println(grid);
-            if (turn = grid.getTurn() % 2 == 0) if ((in = scanner.nextLine()).equals("exit")) break;
-        } while (!grid.play(turn ? Integer.parseInt(in) - 1 : computer.choose()));
-        System.out.println(grid.getTurn() == 9 ? "Draw." : "You " + (turn ? "win" : "lose") + "!");
+            System.out.print(grid);
+
+            int index = 0;
+            if (turn = grid.getTurn() % 2 == 0) {
+                do {
+                    System.out.print(index == INVALID_INPUT
+                            ? "\nSorry, that is not a valid square. Please try again: "
+                            : "\nYou play: ");
+                    index = extractIndex(scanner.nextLine().toLowerCase().trim());
+                } while (index == INVALID_INPUT);
+                if (index == 9) break;
+            } else index = computer.choose();
+            try {
+                isWon = grid.play(index);
+            } catch (IllegalArgumentException ignored) {
+                System.out.println("That square has already been played.");
+            }
+        } while (!isWon);
+        System.out.println();
+        System.out.println(grid.getTurn() == 9 ? "Draw." : "You " + (turn ? "win!" : "lose!"));
 
         scanner.close();
+    }
+
+    private int extractIndex(String input) {
+        int index;
+        try {
+            index = Integer.parseInt(input) - 1;
+            if (index >= 0 && index < 9) return index;
+        } catch (NumberFormatException ignored) {
+            if (input.matches("^(t(op)?|m(id(dle)?)?|b(ot(tom)?)?) ?" +
+                              "(l(eft)?|m(id(dle)?)?|r(ight)?)$")) {
+                char[] sqs = input.toCharArray(); // Square selection.
+                // Accounting for edge cases involving the ‘l’ in ‘middle’, or the ‘m’ in ‘bottom’.
+                int i = 1;
+                if      (sqs[0] == 'm' && sqs.length > 6 && sqs[4] == 'l' && sqs[3] == 'd') i = 5;
+                else if (sqs[0] == 'b' && sqs.length > 6 && sqs[5] == 'm' && sqs[4] == 'o') i = 6;
+                while (sqs[i] != 'r' && sqs[i] != 'm' && sqs[i] != 'l') i++;
+
+                int row = sqs[0] == 't' ? 0 : sqs[0] == 'm' ? 1 : 2;
+                int col = sqs[i] == 'l' ? 0 : sqs[i] == 'm' ? 1 : 2;
+
+                return row * Grid.RWS + col;
+            } else if (input.equals("exit")) {
+                return 9;
+            }
+        }
+        return -1;
     }
 
     public Game(String[] args, InputStream input) {
