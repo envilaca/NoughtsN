51a10c18c68d09fd7973703d7724c1eb9ef31067
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-06-04 12:49:07 +0100

    Changed Main class to run new App class and added aesthetic new line.

--- src/main/java/noughtsn/Crosses.java
+++ src/main/java/noughtsn/Crosses.java
@@ -3,8 +3,9 @@ package noughtsn;
 public class Crosses {
 
     public static void main(String[] args) {
-        Game game = new Game(args, System.in);
-        game.run();
+        App app = new App(args);
+        app.run();
+        System.out.println();
     }
 
 }
