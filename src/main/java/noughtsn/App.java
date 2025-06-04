ddd0b9bad6c5fffca2590d169dda3906700fa206
Eileen Naganuma Vilaça <eileen.vilaca@ten10.com>
2025-06-04 13:07:54 +0100

    A couple of bug fixes.

--- src/main/java/noughtsn/App.java
+++ src/main/java/noughtsn/App.java
@@ -8,13 +8,14 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        if (args.length > 0) if (commands.containsKey(args[0])) commands.get(args[0]).run();
+        if (args.length > 0 && commands.containsKey(args[0])) commands.get(args[0]).run();
         else new Game(args, System.in).run();
     }
 
     public App(String[] args) {
         this.args = args;
-        commands = Map.of("--help", new Help(args));
+        if (args.length == 0) commands = Map.of();
+        else commands = Map.of("--help", new Help(args));
     }
 
     private final String[] args;
